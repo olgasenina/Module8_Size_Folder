c4fd3ac5aed43b66e37ba5adaa8405a7073a6f4e
olgasenina <o.v.senina2020@gmail.com>
2022-01-18 22:35:23 +0300

    Добавьте файлы проекта.

---
+++ Module8_Size_Folder/FolderSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Module8_Size_Folder
+{
+    public static class FolderSettings
+    {
+        /// <summary>
+        /// Считает размер папки на диске (вместе со всеми вложенными папками и файлами). На вход метод принимает URL директории, в ответ — размер в байтах.
+        /// </summary>
+        public static long SizeFolder(DirectoryInfo d)
+        {
+            long size = 0;
+
+            FileInfo[] fis = d.GetFiles();
+            foreach(FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach(DirectoryInfo di in dis)
+            {
+                size += SizeFolder(di);
+            }
+
+            return size;
+        }
+    }
+}
---
+++ Module8_Size_Folder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Module8_Size_Folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите путь к каталогу ");
+
+            DirectoryInfo dirInfo = new DirectoryInfo(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine($"Папка {dirInfo.Name} - размер {FolderSettings.SizeFolder(dirInfo)} байт");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Не удалось рассчитать размер: {e.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
